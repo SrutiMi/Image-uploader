0b11ad874bf0a80a816242a985cf6a6e50cfd2ed
SrutiMi <114284489+SrutiMi@users.noreply.github.com>
2023-06-01 13:01:20 +0530

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path');
+const multer = require('multer');
 
 const app = express();
 
@@ -8,6 +9,27 @@ app.use(express.static(path.join(__dirname,'public')));
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'public','index.html'));
 });
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
+})
+
+const upload = multer({ storage: storage })
+
+app.post('/uploads', upload.single('image'), function (req, res) {
+   
+   res.send('File uploaded successfully');
+});
+
+
+
+
 
 app.listen(3000,()=>{
     console.log('Server is listening on port 3000...');
